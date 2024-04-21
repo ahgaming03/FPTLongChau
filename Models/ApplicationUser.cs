3e14083234931d72674bf6dd1e4e8c6eaacd3c87
ahgaming03 <ahgamingofficial03@gmail.com>
2024-04-21 15:10:31 +0700

    feat: Dashboard layout

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -7,13 +7,13 @@ namespace FPTLongChau.Models
     public class ApplicationUser : IdentityUser
     {
         [StringLength(50)]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
         [StringLength(50)]
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
         [Required]
         [MinLength(5)]
-        public string UserName { get; set; }
-        public DateTime Birthday { get; set; }
-        public Rank Rank { get; set; }
+        public string? UserName { get; set; }
+        public DateTime? Birthday { get; set; }
+        public Rank? Rank { get; set; }
     }
 }
--- wwwroot/Admin/js/demo/chart-area-demo.js
+++ wwwroot/Admin/js/demo/chart-area-demo.js
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [200000, 10000, 5000, 15000090, 10000, 20000, 1500000, 25000, 2000000000000000, 30000, 25000, 4000000],
     }],
   },
   options: {
