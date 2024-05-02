85885367fe2d0fc734b6223f11d6137c21afb30c
HoangIT754 <133497646+HoangIT754@users.noreply.github.com>
2024-05-02 10:53:02 +0700

    feat/Ranks

---
+++ Data/Migrations/20240501082808_imagerank.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FPTLongChau.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class imagerank : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
