9edfeaf3433f3a1aec55870cd9854242543fe7ba
ahgaming03 <ahgamingofficial03@gmail.com>
2024-05-05 19:01:29 +0700

    Feat: Add product to cart

---
+++ wwwroot/js/ShoppingCart.js
@@ -0,0 +1,61 @@
+﻿function SaveCart(cart) {
+    localStorage.setItem("shoppingCart", JSON.stringify(cart));
+}
+
+function LoadCart() {
+    cart = localStorage.getItem("shoppingCart");
+    return cart ? JSON.parse(cart) : [];
+}
+
+function AddItemToCart(id, quantity = 1) {
+    var cart = LoadCart();
+    var found = cart.find(p => p.id === id);
+    if (found === undefined) {
+        cart.push({ id: id, quantity: quantity }); // Add new item to cart
+    } else {
+        found.quantity += quantity; // Increase quantity if item exists
+    }
+    SaveCart(cart);
+}
+
+function RemoveItemFromCart(id) {
+    var cart = LoadCart();
+    var index = cart.findIndex(p => p.id === id);
+    if(index !== -1) {
+        cart.splice(index, 1); // Remove item from cart
+    }
+    SaveCart(cart);
+    LoadCartItems();
+}
+
+function UpdateItemQuantity(id, quantity) {
+    var cart = LoadCart();
+    var found = cart.find(p => p.id === id);
+    if (found !== undefined) {
+        found.quantity = quantity; // Update quantity
+    }
+    SaveCart(cart);
+}
+
+async function LoadCartItems() {
+    let cart = LoadCart();
+
+
+    $.ajax({
+        url: `/Carts/ViewCart/`,
+        type: "POST",
+        contentType: 'application/json',
+        data: JSON.stringify(cart),
+        success: function (result) {
+            console.log(result)
+            $("#cartList").html(result);
+        },
+        error: function (error) {
+            console.log("Error in sending data to server: ", error)
+        }
+    })
+
+}
+document.addEventListener('DOMContentLoaded', function () {
+    LoadCartItems();
+});
