d14dc7634252af53baec0da318bf7241d794b54a
HoangIT754 <133497646+HoangIT754@users.noreply.github.com>
2024-05-06 08:04:51 +0700

    update: continue manage orders

--- Areas/Admin/Controllers/OrdersController.cs
+++ Areas/Admin/Controllers/OrdersController.cs
@@ -35,14 +35,18 @@ namespace FPTLongChau.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var order = await _context.Orders
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (order == null)
+            var ordersDetail = await _context.OrderDetails
+                .Include(od => od.Order)
+                .Include(od => od.Product)
+                .Where(od => od.OrderId == id)
+                .ToListAsync();
+
+            if (ordersDetail == null)
             {
                 return NotFound();
             }
 
-            return View(order);
+            return View(ordersDetail);
         }
 
     
@@ -66,55 +70,19 @@ namespace FPTLongChau.Areas.Admin.Controllers
         // POST: Admin/Orders/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Time,Status")] Order order)
+        [HttpGet]
+        public async Task<IActionResult> ChangeStatus(Guid Id, int Status)
         {
-            if (id != order.Id)
-            {
-                return NotFound();
-            }
+            var order = _context.Orders.FirstOrDefault(m => m.Id == Id);
+            order.Status = Status;
+            _context.Update(order);
+            await _context.SaveChangesAsync();
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(order);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!OrderExists(order.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(order);
+            var orders = _context.Orders;
+
+            return View("Index", orders);
         }
 
-        // GET: Admin/Orders/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var order = await _context.Orders
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            return View(order);
-        }
 
         // POST: Admin/Orders/Delete/5
         [HttpPost, ActionName("Delete")]
